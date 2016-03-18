fbe92734cf90adbb282888947e21e5c584d7a4af
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-18 10:48:58 -0400

    ex11.js

---
+++ ex11.js
@@ -0,0 +1,14 @@
+// print "How old are you?", 
+// age = raw_input()
+// print "How tall are you?",
+// height = raw_input()
+// print "How much do you weigh?",
+// weight = raw_input()
+// 
+// print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+age = prompt("How old are you?");
+height = prompt("How tall are you?");
+weight = prompt("How much do you weigh?");
+
+console.log("So you're " + age + ' old ' + height + " and " + weight + ' heavy'); 
