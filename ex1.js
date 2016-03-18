c095d622a6c5e6f35ddf8240b217124c6c73aa29
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-18 10:22:58 -0400

    first commit

---
+++ ex1.js
@@ -0,0 +1,15 @@
+//print "Hello World!"
+//print "Hello Again"
+//print "I like typing this."
+//print "This is fun."
+//print 'Yay! Printing.'
+//print "I'd much rather you 'not'."
+//print 'I "said" do not touch this.'
+
+console.log("Hello World!");
+console.log("Hello Again");
+console.log("I like typing this.");
+console.log("This is fun.");
+console.log("Yay! Printing");
+console.log("I'd much rather you 'not'.");
+console.log('I "said" do not touch this.');
