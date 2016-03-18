f0979a91a118588d3159c2b1c48fb0d52ead4e73
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-18 10:26:14 -0400

    ex3.js

---
+++ ex3.js
@@ -0,0 +1,55 @@
+//  
+// print "I will now count my chickens:"
+// #line 3 divides 30 / 6 and then adds 25
+// print "Hens", 25 + 30 / 6
+// #line 5 multiples 25 * 3 (75) % 4 (gives you a remander of 3) and 100 - 3 = 97
+// print "Roosters", 100 - 25 * 3 % 4
+// 
+// print "Now I will count the eggs:"
+// #line 9 divides 4 % 2 which leaves a remainder of (0) and 1 / 4 is (.25) so you have 3 + 2 + 1 - 5 + 0 - .25 + 6 = 
+// print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+// #line 11 uses floating point numbers which returns 6.75 because it doesn't round
+// print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 
+// 
+// print "Is it true that 3 + 2 < 5 - 7?"
+// #line 13 is evaluating if 5 < -2 which returns a boolean of False
+// print 3 + 2 < 5 - 7
+// 
+// print "What is 3 + 2?", 3 + 2 #5
+// print "What is 5 - 7?", 5 - 7 # -2
+// 
+// print "Oh, that's why it's False."
+// 
+// print "How about some more." 
+// 
+// print "Is it greater?", 5 > -2 # True
+// print "Is it greater or equal?", 5 >= -2 #True
+// print "Is it less or equal?", 5 <= -2 #False
+ 
+
+console.log("I will now count my chickens:");
+
+console.log("Hens", 25 + 30 / 6);
+
+console.log("Roosters", 100 - 25 * 3 % 4);
+
+console.log("Now I will count the eggs:");
+console.log("3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6");
+console.log(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6);
+
+console.log("3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0");
+console.log(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0);
+
+console.log("Is it true that 3 + 2 < 5 - 7?");
+console.log(3 + 2 < 5 - 7);
+
+console.log("What is 3 + 2?", 3 + 2);
+console.log('What is 5 -7?', 5 - 7);
+
+console.log('Oh, that\'s why it\'s False.');
+
+console.log("How about some more.");
+
+console.log("Is it greater?", 5 > -2);
+console.log("Is it greater or equal?", 5 >= -2);
+console.log("Is it less or equal?", 5 <= -2);
