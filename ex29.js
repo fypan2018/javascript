08fd3282265747d94b40a9b02a99d837333de1b0
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-18 11:16:07 -0400

    ex29.js

---
+++ ex29.js
@@ -0,0 +1,55 @@
+// people = 20
+// cats = 30
+// dogs = 15
+// 
+// if people < cats:
+// 	print "Too many cats! The world is doomed!"
+// 	
+// if people > cats: 
+// 	print "Not many cats! The world is saved!"
+// 	
+// if people < dogs:
+// 	print "The world is drooled on!"
+// 	
+// if people > dogs:
+// 	print "The world is dry!"
+// 	
+// dogs += 5
+// 
+// if people >= dogs:
+// 	print "People are greater than or equal to dogs."
+// 	
+// if people <= dogs: 
+// 	print "People are less than or equal to dogs."
+// 	
+// if people == dogs:
+// 	print "People are dogs."
+// 	
+var people = 20;
+var cats = 30;
+var dogs = 15;
+
+if (people < cats) {
+    console.log("Too many cats! The world is doomed!");  
+}
+if (people > cats) {
+    console.log("Not many cats! The world is saved!");
+}
+if (people < dogs) {
+    console.log("The world is drooled on!");
+}
+if (people > dogs) {
+    console.log("The world is dry!");
+}
+
+dogs += 5;
+
+if (people >= dogs) {
+    console.log("People are greater than or equal to dogs.");
+}
+if (people <= dogs) {
+    console.log("People are less than or equal to dogs");
+}
+if (people == dogs) {
+    console.log("People are dogs.");
+}
