2ee1faf6078739cd3dce36783e2281a26947f249
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-18 10:35:00 -0400

    ex7.js

---
+++ ex7.js
@@ -0,0 +1,47 @@
+// print "Mary had a little lamb."
+// #prints "Its fleece was white as snow."
+// print "Its fleece was white as %s." % 'snow'
+// print "And everywhere that Mary went."
+// #prints ..........
+// print "." * 10 #what's that do?
+// 
+// end1 = "C"
+// end2 = "h"
+// end3 = "e"
+// end4 = "e"
+// end5 = "s"
+// end6 = "e"
+// end7 = "B"
+// end8 = "u"
+// end9 = "r"
+// end10 = "g"
+// end11 = "e"
+// end12 = "r"
+// 
+// #watch that comma at the end. try removing it it see what happens
+// #prints Cheese Burger on one line with the  , and on two line without the ,
+// print end1 + end2 + end3 + end4 + end5 + end6,
+// print end7 + end8 + end9 + end10 + end11 + end12
+
+console.log("Mary had a little lamb.");
+
+console.log("Its fleece was white as snow.");
+
+console.log("And everywhere that Mary went.");
+
+console.log('.'.repeat(10));
+
+var end1 = "C";
+var end2 = "h"; 
+var end3 = 'e';
+var end4 = 'e'; 
+var end5 = 's';
+var end6 = 'e';
+var end7 = "B"; 
+var end8 = 'u';
+var end9 = 'r';
+var end10 = 'g';
+var end11 = 'e';
+var end12 = 'r';
+
+console.log(end1 + end2 + end3 + end4 + end5 + end6 + ',') console.log(end7 + end8 + end9 + end10 + end11 + end12);
