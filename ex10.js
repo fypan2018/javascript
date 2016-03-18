a6cd996784aa90cc9e4d9a1d89d6504381a4fa24
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-18 10:47:14 -0400

    ex10.js

---
+++ ex10.js
@@ -0,0 +1,30 @@
+// tabby_cat = "\tI'm tabbed in."
+// persian_cat = "I'm split\non a line."
+// backslash_cat = "I'm a \\ a \\ cat."
+// 
+// fat_cat = """
+// I'll do a list:
+// \t* Cat food
+// \t* Fishies
+// \t* Catnip\n\t* Grass
+// """
+// 
+// print tabby_cat
+// print persian_cat
+// print backslash_cat
+// print fat_cat
+
+tabbyCat = "\tI'm tabbed in.";
+persianCat = "I'm split\non a line.";
+backslashCat = "I'm a \\ a \\ cat.";
+
+fatCat = "\n\
+I'll do a list:\n\
+\t* Cat food\n\
+\t* Fishies\n\
+\t* Catnip\n\t* Grass"
+
+console.log(tabbyCat);
+console.log(persianCat);
+console.log(backslashCat);
+console.log(fatCat);
