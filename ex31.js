7e43ce4a190cfa0750e5fd0347441c5351fda6ac
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-26 14:20:54 -0400

    python_to_js ex31.js

--- ex31.js
+++ ex31.js
@@ -32,37 +32,49 @@
 // else:
 // 	print "You stumble around and fall on a knife and die. Good job!"
 // 	
-var door = prompt("You enter a dark room with two doors. Do you go through door #1 or door #2?");
 
-if (door == "1") {
-    console.log("There's a giant bear here eating a cheese cake. What do you do?");
-    console.log("1. Take the cake.");
-    console.log("2. Scream at the bear.");
-    
-    var bear = prompt("Do you pick #1 or #2");
-    
-    if (bear == "1") {
-        console.log("The bear eats your face off. Good job!");
-    } else if (bear == "2") {
-        console.log("The bear eats your legs off. Good job!");
-    } else {
-        console.log("Well, doing " + bear + " is probably better. Bear runs away."); 
+var prompt = require('prompt');
+
+prompt.start();
+
+console.log("You enter a dark room with two doors. Do you go through door #1 or door #2?");
+
+prompt.get('door', function (err, result) {
+    if (result.door == "1") {
+        console.log("There's a giant bear here eating a cheese cake. What do you do?");
+        console.log("1. Take the cake.");
+        console.log("2. Scream at the bear.");
+       
+        console.log("Do you pick #1 or #2");
+        
+        prompt.get('bear', function (err, result) {
+
+        if (result.bear == "1") {
+            console.log("The bear eats your face off. Good job!");
+        } else if (result.bear == "2") {
+            console.log("The bear eats your legs off. Good job!");
+        } else {
+            console.log("Well, doing " + result.bear + " is probably better. Bear runs away."); 
+        }
+    		});
+	} else if (result.door == "2") {
+        console.log("You stare into the endless abyss at Cthulhu's retina.");
+        console.log("1. Blueberries.");
+        console.log("2. Yellow jacket clothespins.");
+        console.log("3. Understanding revolvers yelling melodies.");
+     
+        console.log("Do you pick #1, #2 or #3");
+        
+        prompt.get('insanity', function (err, result) {
+     
+        	if (result.insanity == '1' || result.insanity == '2') {
+            	console.log("Your body survives powered by a mind of jello. Good Job!");
+        	} else  {
+            	console.log("The insanity rots your eyes into a pool of muck. Good Job!");
+			}
+		});
+					
+	} else {
+     	console.log("You stumble around and fall on a knife and die. Good Job!");
     }
-} else if (door == "2") {
-    console.log("You stare into the endless abyss at Cthulhu's retina.");
-    console.log("1. Blueberries.");
-    console.log("2. Yellow jacket clothespins.");
-    console.log("3. Understanding revolvers yelling melodies.");
-    
-    var insanity = prompt("Do you pick #1, #2 or #3");
-    
-    if (insanity == '1' || insanity == '2') {
-        console.log("Your body survives powered by a mind of jello. Good Job!");
-    } else {
-        console.log("The insanity rots your eyes into a pool of muck. Good Job!");
-    }
-} else {
-    console.log("You stumble around and fall on a knife and die. Good Job!");
-}
-
-
+});
